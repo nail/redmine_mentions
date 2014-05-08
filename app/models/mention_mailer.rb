fceda5bc61bd1de97ed66950b4b65f4cce295744
Renaud Drousies <renaud.drousies@syspay.com>
2014-05-08 10:17:30 +0000

    ruby 1.8 fixes + mail content/subject

--- app/models/mention_mailer.rb
+++ app/models/mention_mailer.rb
@@ -1,6 +1,6 @@
 class MentionMailer < ActionMailer::Base
   layout 'mailer'
-  default from: Setting.mail_from
+  default :from => Setting.mail_from
   def self.default_url_options
     Mailer.default_url_options
   end
@@ -9,6 +9,8 @@ class MentionMailer < ActionMailer::Base
   def notify_mentioning(issue, journal, user)
     @issue = issue
     @journal = journal
-    mail(to: user.mail, subject: "You were mentioned in a note")
+    @issue_url = url_for(:controller => 'issues', :action => 'show', :id => issue)
+    mail :to => user.mail,
+        :subject => "You were mentioned in a note of ##{issue.id} - #{issue.subject} (#{issue.status.name})"
   end
 end
